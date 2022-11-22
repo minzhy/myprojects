145a7af138b60663cea21956c86c0fcdfcb90fa4
minzhy <1324537990@qq.com>
2022-11-22 11:05:19 +0800

    finish boolean

--- ast/ast.go
+++ ast/ast.go
@@ -167,3 +167,13 @@ func (ie *InfixExpression) String() string {
 
 	return out.String()
 }
+
+// expression statement
+type Boolean struct {
+	Token token.Token
+	Value bool
+}
+
+func (b *Boolean) expressionNode()      {}
+func (b *Boolean) TokenLiteral() string { return b.Token.Literal }
+func (b *Boolean) String() string       { return b.Token.Literal }
